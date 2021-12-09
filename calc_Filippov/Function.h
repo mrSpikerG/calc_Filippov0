ab663e16480c0ca1413bf695c6cc84a32807ff83
student <student@05-02>
2021-12-09 20:15:51 +0200

    add * and / funcs

--- calc_Filippov/Function.h
+++ calc_Filippov/Function.h
@@ -7,3 +7,11 @@ inline int plus(int firstNum, int secondNum) {
 inline int minus(int firstNum, int secondNum) {
 	return firstNum - secondNum;
 }
+
+inline int mul(int firstNum, int secondNum) {
+	return firstNum * secondNum;
+}
+
+inline int div(int firstNum, int secondNum) {
+	return firstNum / secondNum;
+}
